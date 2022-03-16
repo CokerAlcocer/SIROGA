773e9147c400275071b4fbbdb86114f27adc62e7
DiegoAraithVillalobosHernandez <20203tn063@utez.edu.mx>
2022-03-16 00:29:15 -0600

    Se completo servicio Sistem

--- service/src/main/java/project/siroga/sistem/controller/SistemController.java
+++ service/src/main/java/project/siroga/sistem/controller/SistemController.java
@@ -1,4 +1,56 @@
 package project.siroga.sistem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.siroga.sistem.model.Sistem;
+import project.siroga.utils.Message;
+
+@RestController
+@RequestMapping("/siroga/api/sistem")
+@CrossOrigin(origins = {"*"})
 public class SistemController {
+    @Autowired
+    SistemService sistemService;
+
+    @GetMapping("/")
+    public ResponseEntity<Message> getAll(){
+        return sistemService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Message> getById(@PathVariable("id") long id){
+        return sistemService.findById(id);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Message> saveSistem(@RequestBody SistemDTO sistemDTO){
+        return sistemService.save(new Sistem(
+                sistemDTO.getBrokerLink(),
+                sistemDTO.getHumEarth(),
+                sistemDTO.getHumAir(),
+                sistemDTO.getTempEarth(),
+                sistemDTO.getTempAir(),
+                sistemDTO.getUser(),
+                null,
+                sistemDTO.getStatus(),
+                null
+        ));
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Message> updateSistem(@RequestBody SistemDTO sistemDTO){
+        return sistemService.update(new Sistem(
+                sistemDTO.getId(),
+                sistemDTO.getBrokerLink(),
+                sistemDTO.getHumEarth(),
+                sistemDTO.getHumAir(),
+                sistemDTO.getTempEarth(),
+                sistemDTO.getTempAir(),
+                sistemDTO.getUser(),
+                null,
+                sistemDTO.getStatus(),
+                null
+        ));
+    }
 }
--- service/src/main/java/project/siroga/sistem/controller/SistemService.java
+++ service/src/main/java/project/siroga/sistem/controller/SistemService.java
@@ -1,4 +1,65 @@
 package project.siroga.sistem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.siroga.sistem.model.Sistem;
+import project.siroga.sistem.model.SistemRepository;
+import project.siroga.utils.Message;
+
+import java.sql.SQLException;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class SistemService {
+    @Autowired
+    SistemRepository sistemRepository;
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<Message> findAll(){
+        return new ResponseEntity<>(new Message("OK", false, sistemRepository.findAll()),
+                HttpStatus.OK);
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<Message> findById(Long id){
+        if (sistemRepository.existsById(id)) {
+            return new ResponseEntity<>(new Message("Encontrado", false, sistemRepository.findById(id)),
+                    HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new Message("No encontrado", true, null),HttpStatus.BAD_REQUEST);
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<Message> findByBrokerLink(String brokerLink){
+        if (sistemRepository.existsByBrokerLink(brokerLink)) {
+            return new ResponseEntity<>(new Message("OK", false, sistemRepository.findByBrokerLink(brokerLink)),
+                    HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new Message("No encontrado", true, null), HttpStatus.BAD_REQUEST);
+    }
+
+    @Transactional(rollbackFor = {SQLException.class})
+    public ResponseEntity<Message> save(Sistem sistem){
+        Optional<Sistem> existingSistem = sistemRepository.findByBrokerLink(sistem.getBrokerLink());
+        if (existingSistem.isPresent()){
+            return new ResponseEntity<>(new Message("El sistema ya existe",true, null), HttpStatus.BAD_REQUEST);
+        }
+        Sistem savedSistem = sistemRepository.saveAndFlush(sistem);
+        return new ResponseEntity<>(new Message("Sistema registrado", false, savedSistem), HttpStatus.OK);
+    }
+
+    @Transactional(rollbackFor = {SQLException.class})
+    public ResponseEntity<Message> update(Sistem sistem){
+        if (sistemRepository.existsByBrokerLink(sistem.getBrokerLink()) || sistemRepository.existsById(sistem.getId())) {
+            return new ResponseEntity<>(new Message("Sistema actualizado", false, sistemRepository.saveAndFlush(sistem)),
+                    HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new Message("Sistema no encontrado", true, null), HttpStatus.BAD_REQUEST);
+    }
+
+
 }

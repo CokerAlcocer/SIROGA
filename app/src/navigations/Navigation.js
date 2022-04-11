f14cfd4d014c2f0d6f27ee4ed2c4303f19953e11
Gidggidonni98 <20193tn178@utez.edu.mx>
2022-04-11 17:57:57 -0500

    Se quita la barra en el Login
    Se quita la barra en el Login

--- app/src/navigations/Navigation.js
+++ app/src/navigations/Navigation.js
@@ -1,60 +1,90 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Icon } from "react-native-elements";
 import IndexStack from "./IndexStack";
 import SystemStack from "./SystemStack";
 import ProfileStack from "./ProfileStack";
-import Login from "../screens/Login";
-import Index from "../screens/Index";
+import { firebaseApp } from "../utils/firebase";
+import * as firebase from "firebase";
 
 const Tab = createBottomTabNavigator();
 export default function Navigation() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="index"
-        tabBarOptions={{
-          headerShown: false,
-          inactiveTintColor: "white",
-          activeTintColor: "green",
-        }}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ color }) => screenOption(route, color),
-        })}
-      >
-        <Tab.Screen
-          name="index"
-          component={IndexStack}
-          options={({ route }) => ({
-            tabBarVisible: getTabBarVisibility(route),
+  const [login, setLogin] = useState(null)
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      !user ? setLogin(false) : setLogin(true);
+    });   
+  }, [login]);
+
+  if (login) {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="index"
+          tabBarOptions={{
+            headerShown: false,
+            inactiveTintColor: "white",
+            activeTintColor: "green",
+          }}
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color }) => screenOption(route, color),
           })}
-        />
-        <Tab.Screen
-          name="system"
-          component={SystemStack}
-          options={{ title: "Sistemas" }}
-        />
-        <Tab.Screen
-          name="profile"
-          component={ProfileStack}
-          options={{ title: "Perfil" }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
-
-
-getTabBarVisibility = (route) => {
-  const routeName = route.name;
-
-  if (routeName === "index") {
-    return true;
+        >
+          <Tab.Screen
+            name="index"
+            component={IndexStack}
+            options={({ route }) => ({
+              
+            })}
+          />
+            <Tab.Screen
+            name="system"
+            component={SystemStack}
+            options={{ title: "Sistemas" }}
+          />
+          <Tab.Screen
+            name="profile"
+            component={ProfileStack}
+            options={{ title: "Perfil" }}
+          />
+          
+          
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="index"
+          tabBarOptions={{
+            headerShown: false,
+            inactiveTintColor: "white",
+            activeTintColor: "green",
+          }}
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color }) => screenOption(route, color),
+          })}
+        >
+          <Tab.Screen
+            name="index"
+            component={IndexStack}
+            options={({ route }) => ({
+              tabBarVisible: false,
+            })}
+         
+          />
+          
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
   }
 
-  return false;
-};
+ 
+}
+
 
 function screenOption(route, color) {
   let icono;
